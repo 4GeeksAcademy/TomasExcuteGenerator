cabae45b8ab103bf6791e807413efeddd66e31d1
tomasmj503 <86091325+tomasmj503@users.noreply.github.com>
2023-07-11 22:56:09 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+
+  let randomAction = who[Math.floor(Math.random() * action.length)];
+  console.log(randomWho + " " + randomAction);
 };
